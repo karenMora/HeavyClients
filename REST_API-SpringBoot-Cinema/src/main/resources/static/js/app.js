e4f0779296f23924ae9f21fa95b177f7abedd9cc
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-03-21 10:09:05 -0500

    adelanto

--- REST_API-SpringBoot-Cinema/src/main/resources/static/js/app.js
+++ REST_API-SpringBoot-Cinema/src/main/resources/static/js/app.js
@@ -1,27 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//@author dsaavedra
+app = (function () {
 
-apimock=(function(){
+    return{
+        getCinemaByName: function (name) {
 
-	var mockdata=[];
-
-
-
-   mockdata["Cine80"]=[{"name":"Cine80","functions":[{"movie":{"name":"Titanic Movie","genre":"Action"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:30"},{"movie":{"name":"The Purge","genre":"Horror"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:30"}]}];
-
-   mockdata["Cine112"]=[{"name":"Cine112","functions":[{"movie":{"name":"mision imposible","genre":"Fiction"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:31"}]}];
-
-
-	return {
-		getCinemaByName:function(name,callback){
-			callback(
-				mockdata[name]
-			);
-		}
-	}	
+            return apimock.getCinemaByName(name, function (cine) {
+                for (var i = 0; i < cine[0].functions.length; i++) {
+                    $("#ccine").append("<tr><td>" + cine[0].name + "</td><td>" + cine[0].functions[i].movie.name + "</td><td>" + cine[0].functions[i].seats.length + "</td><td>" + cine[0].functions[i].date + "</td>")
+                }
+                //console.log(cine);
+            });
+        }
+    }
 
 })();
